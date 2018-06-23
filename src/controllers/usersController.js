17970b8068fb59b25db2f9e8831769b4ca66c372
Gladkowski <marcingladkowski@gmail.com>
2018-06-23 09:20:53 +0200

    add cross domain headers

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -2,15 +2,19 @@ import User from '../models/user';
 
 export default {
         async create(req, res) {
-            const user = await new User({
-                email: req.body.email,
-                username: req.body.username,
-                password: req.body.password,
-                passwordConf: req.body.password
-            }).save();
 
-            console.log(user);
+            console.log(res.body)
+        
 
-        return res.status(201).send({data: user, 'status': 'ok'});
+            // const user = await new User({
+            //     email: req.body.email,
+            //     username: req.body.username,
+            //     password: req.body.password,
+            //     passwordConf: req.body.password
+            // }).save();
+
+            // console.log(user);
+
+        return res.status(201).send({'status': 'ok'});
     }
 }
--- src/index.js
+++ src/index.js
@@ -24,6 +24,12 @@ mongoose.connection.on('error', (err) => {
 
 const app = express();
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
