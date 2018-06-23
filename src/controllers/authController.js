89a0e71f7fb60cb32957e4c33a18826cc825844f
Gladkowski <marcingladkowski@gmail.com>
2018-06-23 11:59:02 +0200

    use JWT token for login - response to user

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,10 +1,24 @@
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+import User from '../models/user';
 
 export default {
     async login(req, res) {
 
-        console.log(res.body);
+        User.findOne({ email: req.body.email }, function (err, user) {
 
-        return res.send({'status': 'Zalogowano!'});
+            if (err) return res.status(500).send('Error on the server.');
+            if (!user) return res.status(404).send('No user found.');
+
+            var passwordIsValid = bcrypt.compareSync(req.body.password, user.password);
+
+            if (!passwordIsValid) return res.status(401).send({ auth: false, token: null });
+            var token = jwt.sign({ id: user._id }, 'marcinwrc123', {
+                expiresIn: 86400 // expires in 24 hours
+            });
+            res.status(200).send({ auth: true, token: token });
+        });
+
 
     }
 };
