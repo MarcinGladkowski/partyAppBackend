18eda14719237411b25c45216c4c81ad609a37fb
Gladkowski <marcingladkowski@gmail.com>
2018-07-11 00:04:05 +0200

    add comments to methods

--- src/controllers/partyController.js
+++ src/controllers/partyController.js
@@ -1,6 +1,7 @@
 import Party from '../models/party';
 
 export default {
+    /** create new party */
     async create(req, res) {
 
         const party = await new Party({
@@ -11,19 +12,16 @@ export default {
             userCreated: req.userId
             
         }).save();
+
         return res.send({'data': party, 'status': 'ok'})
     },
-
+    /** get all parties */
     async list(req, res) {
-
-        // await Party.find({}, function(err, parties) {
-        //     const partyList = parties;
-        //     return res.send({'parties': partyList});
-        // });
 
         await Party.find({}).populate('userCreated').exec(function(error, parties) {
             const partyList = parties;
             return res.send({'parties': partyList});
         });
+        
     }
 }
