aa8fef3384f5a787f3eb4e24d93e4c101313c502
Marcin Gładkowski <mgladkowski@swift.pl>
2018-09-11 22:59:11 +0200

    add partyType controller

---
+++ src/controllers/partyTypeController.js
@@ -0,0 +1,13 @@
+import PartyType from '../models/partyType';
+
+export default {
+    /** create new party */
+    async create(req, res) {
+
+        return res.send({'data': party, 'status': 'ok'})
+    },
+    /** get all parties */
+    async list(req, res) {
+
+    }
+}
