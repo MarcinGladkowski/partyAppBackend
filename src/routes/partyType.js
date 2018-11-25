210cac05c867bc5261979f2685de01c3ae43a105
Gladkowski <marcingladkowski@gmail.com>
2018-11-25 18:59:14 +0100

    add middleware to save files

--- src/routes/partyType.js
+++ src/routes/partyType.js
@@ -2,13 +2,17 @@ import { Router }  from 'express';
 import partyTypeController from '../controllers/partyTypeController';
 import { catchAsync }  from '../middlewares/errors';
 import verifyToken from '../auth/authentication';
+import upload from '../middlewares/multer';
 
 export default () => {
     const api = Router();
+    // const upload = multer({dest: 'uploads/'});
 
     api.post('/', catchAsync(partyTypeController.create));
 
     api.get('/', catchAsync(partyTypeController.list));
+
+    api.post('/upload', upload.single('icon'), catchAsync(partyTypeController.upload));
 
     return api;
 }
