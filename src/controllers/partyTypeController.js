72247e96454c2228eb5ef9f19e1d8c352642907c
Gladkowski <marcingladkowski@gmail.com>
2018-11-25 18:58:37 +0100

    modify path to save files

--- src/controllers/partyTypeController.js
+++ src/controllers/partyTypeController.js
@@ -1,15 +1,35 @@
 import PartyType from '../models/partyType';
 
+
 export default {
     /** create new party */
     async create(req, res) {
 
-
-
         return res.send({'status': 'ok'})
     },
+
     /** get all parties */
     async list(req, res) {
 
+    },
+
+    async upload(req, res) {
+        if (!req.file) {
+            console.log("No file received");
+            return res.send({
+              success: false
+            });
+        
+          } else {
+            console.log('file received');
+
+            const image = {};
+            image.url = req.file.url;
+            image.id = req.file.public_id;
+
+            return res.send({
+              success: true
+            })
+          }
     }
 }
