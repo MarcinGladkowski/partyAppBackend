dfa208a33a4f53ffc9bd05245e1ac73d25461f47
Gladkowski <marcingladkowski@gmail.com>
2018-12-05 18:06:27 +0100

    add get user method

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,13 +1,14 @@
 import { Router }  from 'express';
 import usersController from '../controllers/usersController';
 import { catchAsync }  from '../middlewares/errors';
+import verifyToken from '../auth/authentication';
 
 export default () => {
     const api = Router();
 
     api.post('/', catchAsync(usersController.create));
     api.post('/activate', catchAsync(usersController.activate));
-    api.get('/', catchAsync(usersController.get));
+    api.get('/', verifyToken, catchAsync(usersController.findOne));
 
     return api;
 }
