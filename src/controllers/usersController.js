f20c49d881347b33f9cf409c81eb11326a5959f8
Gladkowski <marcingladkowski@gmail.com>
2018-12-22 22:20:24 +0100

    avatar upload path

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -1,56 +1,53 @@
 import User from '../models/user';
 import Mailer from '../utils/mailer/mailer';
-import { hashString } from '../utils/password';
-
+import PartyType from "../models/partyType";
 
 export default {
-        async create(req, res) {
+    async create(req, res) {
+        const {email, username, password} = req.body;
+        const user = await new User({email, username, password}).save();
+        new Mailer(user).sendEmail();
+        return res.status(201).send(user);
+    },
 
-            const { email, username, password } = req.body;
-        
-            const user = await new User({ email, username, password }).save(); 
+    /** Active user from email hash */
+    async activate(req, res) {
+        User.findOneAndUpdate({hash: req.body.hash}, {$set: {active: true}}, {new: true}, (err, user) => {
+            if (err) {
+                return res.status(200).send({'status': 'error'});
+            }
+            if (!user) {
+                return res.status(200).send({'status': 'error'});
+            }
+            return res.status(201).send(user);
+        });
+    },
 
-            new Mailer(user).sendEmail();
+    async findOne(req, res) {
+        const user = await User.findById(req.userId, function (err, user) {
+        });
+        return res.status(200).send(user);
+    },
 
-            return res.status(201).send({'data': user, 'status': 'ok'});
-        },
-        
-        /**
-         * Active user from email hash
-         */
-        async activate(req, res) {
-            
-            User.findOneAndUpdate({hash: req.body.hash}, {$set:{active:true}}, {new: true}, function(err, user){
-
-                if (err) { return res.status(200).send({'status': 'error'}); }
-
-                if (user === null){ return res.status(200).send({'status': 'error'}); }
-            
-                return res.status(201).send({'status': 'ok'});
+    async update(req, res) {
+        const {email, username} = req.body;
+        await User.findByIdAndUpdate(req.userId, {
+            $set: {
+                username: username,
+                email: email
+            }
+        }, {new: true}, (err, user) => {
+            return res.status(200).send(user);
+        });
+    },
+    /** Update avatar for user */
+    async updateAvatar(req, res) {
+        if (req.file) {
+            await User.findByIdAndUpdate(req.userId, {$set: {avatar: req.file.filename}}, {new: true}, (err, user) => {
+                return res.status(200).send(user);
             });
-
-        },
-
-        /**  action actualy for testing code */
-        async findOne(req, res) {
-
-            const user = await User.findById(req.userId, function(err, user){});
-
-            return res.status(200).send(user);
-        },
-
-        async update(req, res) {
-
-            const { email, username } = req.body;
-
-            const user = await User.findById(req.userId, function(err, user){});
-            if (!user) return next();
-
-            user.email = email;
-            user.username = username;
-            await user.save();
-
-            return res.status(200).send(user);
+        } else {
+            return res.send({'status': 'error'})
         }
-
+    }
 }
