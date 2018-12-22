8a7ef8bd9592a2eca12e70af4de7989da43a665b
Gladkowski <marcingladkowski@gmail.com>
2018-12-22 22:19:53 +0100

    upload file to each dir

--- src/middlewares/multer.js
+++ src/middlewares/multer.js
@@ -1,7 +1,9 @@
 import multer from 'multer';
 
 const storage = multer.diskStorage({
-    destination: '../partyApp/src/assets/icons',
+    destination: (req, file, cb) => {
+        cb(null, `${__dirname}/../../public/${file.fieldname}`)
+    },
     filename: (req, file, cb) => {
         cb(null,`${new Date().getTime()}-${file.originalname}`)
     }
