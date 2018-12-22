a666f155ac8140e41e5e252ca17cc94388eb9f25
Gladkowski <marcingladkowski@gmail.com>
2018-12-22 22:18:09 +0100

    add comments and avatar update path

--- src/routes/users.js
+++ src/routes/users.js
@@ -2,14 +2,21 @@ import { Router }  from 'express';
 import usersController from '../controllers/usersController';
 import { catchAsync }  from '../middlewares/errors';
 import verifyToken from '../auth/authentication';
+import upload from '../middlewares/multer';
 
 export default () => {
     const api = Router();
-
+    /** create new user */
     api.post('/', catchAsync(usersController.create));
+    /** activate user by hash*/
     api.post('/activate', catchAsync(usersController.activate));
+    /** get user data */
     api.get('/', verifyToken, catchAsync(usersController.findOne));
+    /** update main user data */
     api.put('/', verifyToken, catchAsync(usersController.update));
+    /** update user avatar */
+    api.post('/avatar', verifyToken, upload.single('avatar'),  catchAsync(usersController.updateAvatar));
+
 
     return api;
 }
