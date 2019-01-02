65d12f4c660de9b1d981ad87ae3e712e720607ed
Gladkowski <marcingladkowski@gmail.com>
2019-01-02 23:49:36 +0100

    party invite routes

---
+++ src/routes/partyInvite.js
@@ -0,0 +1,16 @@
+import { Router }  from 'express';
+import verifyToken from '../auth/authentication';
+import { catchAsync }  from '../middlewares/errors';
+import partyInviteController from '../controllers/partyInviteController';
+
+export default () => {
+    const api = Router();
+
+    api.post('/', catchAsync(partyInviteController.create));
+
+    api.get('/:partyId', catchAsync(partyInviteController.findByParty));
+
+    api.post('/send', catchAsync(partyInviteController.sendInvite));
+
+    return api;
+}
