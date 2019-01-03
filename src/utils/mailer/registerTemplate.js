07cb704d28b3910dc5df20a63d316f83b972d3c5
Gladkowski <marcingladkowski@gmail.com>
2019-01-03 13:51:51 +0100

    class for register email

---
+++ src/utils/mailer/registerTemplate.js
@@ -0,0 +1,15 @@
+export default class RegisterTemplate {
+
+    constructor(user) {
+        this.user = user;
+    }
+
+    makeTemplate() {
+        this.subject = `Cześć ${this.user.username} Zarejestrowałeś się w aplikacji Imprezownia!`;
+        this.text = 'potwierdź aktywację konta';
+        this.html = `<p>Potwierdź link aktywujący i zaloguj się do aplikacji!</p>
+                     <a href="http://localhost:4200/action/activate/${this.user.hash}">Aktywuj konto</a>`;
+
+        return this;
+    }
+}
