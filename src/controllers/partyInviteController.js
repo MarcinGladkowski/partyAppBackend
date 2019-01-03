b58c4780d121c7699b5cd77894eb25cf290fe8b8
Gladkowski <marcingladkowski@gmail.com>
2019-01-03 02:12:04 +0100

    send invite email - first changes

--- src/controllers/partyInviteController.js
+++ src/controllers/partyInviteController.js
@@ -1,24 +1,34 @@
 import PartyInvite from '../models/partyInvite';
+import Mailer from "../utils/mailer/mailer";
+import EmailTemplate from "../utils/mailer/emailTemplate";
 
 export default {
     async create(req, res) {
-
         const { party, userInvited } = req.body;
         const partyInvite = await PartyInvite.create({userInvited, party});
-
-        return res.status(200).send(partyInvite);
+        const partySaved = await partyInvite.populate("userInvited").execPopulate();
+        return res.status(200).send(partySaved);
     },
-    findByParty: async function (req, res) {
 
+    async findByParty(req, res) {
         const partyInvites = await PartyInvite.find({party: req.params.partyId}).populate('userInvited');
         return res.status(200).send(partyInvites);
-
     },
+
     async sendInvite(req, res) {
+        const partyInviteId = req.params.Id;
+        const { send } = req.body;
 
-        const { partyId, userInvitedId } = req.body;
+        const partyInvite = await PartyInvite.findByIdAndUpdate({_id: partyInviteId}, {send: send}).populate("userInvited");
+        // @TODO send email
+        const user = partyInvite.userInvited;
+        const subject = `Cześć ${user.username} Zarejestrowałeś się w aplikacji Imprezownia!`,
+            text = 'potwierdź aktywację konta',
+            html = `<p>Potwierdź link aktywujący i zaloguj się do aplikacji!</p>
+                   <a href="http://localhost:4200/action/activate/${user.hash}"/>Aktywuj konto</a>`;
 
-        const partyInvite = await PartyInvite.findOneAndUpdate({party: partyId, userInvited: userInvitedId }, {send: true});
+        const inviteEmail = new EmailTemplate(subject, text, html);
+        new Mailer(user).layout(inviteEmail).sendEmail();
 
         return res.status(200).send(partyInvite);
     }
