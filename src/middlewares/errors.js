69d975581c88ae89915e410b831e62efc9eab222
Gladkowski <marcingladkowski@gmail.com>
2019-01-03 02:11:10 +0100

    refactor console message

--- src/middlewares/errors.js
+++ src/middlewares/errors.js
@@ -12,5 +12,5 @@ export function catchAsync(fn) {
 
 export function catchErrors(err, req, res, next) {
     res.status(err.status || 500);
-    console.log(`Catch error in middleware ${err.message}`);
+    console.log(`Error: ${err.message}`);
 }
