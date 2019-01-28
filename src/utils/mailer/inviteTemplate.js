b3ecfac2feeae1b58fdd2b6b3a835c84b74b35df
Marcin <marcingladkowski@gmail.com>
2019-01-28 20:49:55 +0100

    change property config ip in email template

--- src/utils/mailer/inviteTemplate.js
+++ src/utils/mailer/inviteTemplate.js
@@ -11,7 +11,7 @@ export default class InviteTemplate {
         this.subject = `Cześć ${this.user.username} Zostałeś zaproszony na wydarzenie!`;
         this.text = 'Kliknij w link aby wyświetlić szczegóły i dołączyć do wydarzenia';
         this.html = `<p>Potwierdź link aktywujący i zaloguj się do aplikacji!</p>
-                     <a href="http://${config.server.port}:4200/party/details/${this.party._id}">Zobacz wydarzenie</a>`;
+                     <a href="http://${config.server.ip}:4200/party/details/${this.party._id}">Zobacz wydarzenie</a>`;
         return this;
     }
 }
--- src/utils/mailer/registerTemplate.js
+++ src/utils/mailer/registerTemplate.js
@@ -1,3 +1,5 @@
+import config from '../../config/config';
+
 export default class RegisterTemplate {
 
     constructor(user) {
@@ -8,7 +10,7 @@ export default class RegisterTemplate {
         this.subject = `Cześć ${this.user.username} Zarejestrowałeś się w aplikacji Imprezownia!`;
         this.text = 'potwierdź aktywację konta';
         this.html = `<p>Potwierdź link aktywujący i zaloguj się do aplikacji!</p>
-                     <a href="http://localhost:4200/action/activate/${this.user.hash}">Aktywuj konto</a>`;
+                     <a href="http://${config.server.ip}:4200/action/activate/${this.user.hash}">Aktywuj konto</a>`;
 
         return this;
     }
